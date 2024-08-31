63798eda887203342dec1d8661a83bd9ebee2a9a
Arely Rivera-Gonzalez <arely-rivera@Arelys-MacBook-Pro.local>
2024-08-31 14:20:04 -0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/personality-gen",
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
