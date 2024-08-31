92b4f3973342b88338e072b6c5306b75147ab1cc
Arely Rivera-Gonzalez <arely-rivera@Arelys-MacBook-Pro.local>
2024-08-31 15:03:18 -0700

    added questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,82 +1,184 @@
 export const questions = [
   {
     _id: "1",
-    question: "Which activity do you enjoy the most?",
+    question: "What genre do you enjoy reading most?",
     choices: [
-      { answer: "Reading a book", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Going for a run", personalityType: "Energetic Type", points: 1 },
-      { answer: "Volunteering", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Competing in a game", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Sappy/saucy romance", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Contemporary / realistic fiction", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Poetry", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Thriller / mystery", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Graphic novel / fantasy", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Historical / philosophical", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Horror", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Humor", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "2",
-    question: "What is your favorite time of day?",
+    question: "What animal resonates with you most?",
     choices: [
-      { answer: "Morning", personalityType: "Energetic Type", points: 1 },
-      { answer: "Afternoon", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Evening", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Night", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Otter", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Bear", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Mouse", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Alligator", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Dragon", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Elephant", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Tiger", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Fox", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "3",
-    question: "What is your favorite animal?",
+    question: "What color resonates with you most?",
     choices: [
-      { answer: "Cat", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Dog", personalityType: "Energetic Type", points: 1 },
-      { answer: "Panda", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Alligator", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Red", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Green", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Blue", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Yellow", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Purple", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "White", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Black", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Orange", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "4",
-    question: "How do you handle stress?",
+    question: "Choose one word to describe yourself:",
     choices: [
-      { answer: "Meditation", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Physical exercise", personalityType: "Energetic Type", points: 1 },
-      { answer: "Helping others", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Confronting the issue head-on", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Romantic", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Bitter", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Shy", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Emotional", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Playful", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Intellectual", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Aggressive", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Confident", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "5",
-    question: "What type of movies do you prefer?",
+    question: "Pick a tarot card:",
     choices: [
-      { answer: "Romantic dramas", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Action-packed thrillers", personalityType: "Energetic Type", points: 1 },
-      { answer: "Psychological thrillers", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Horror", personalityType: "Soul Muncher", points: 1 }
+      { answer: "The lovers", personalityType: "Dust Bunny", points: 1 },
+      { answer: "The emperor", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "The Hermit", personalityType: "Dust Bunny", points: 1 },
+      { answer: "The Tower", personalityType: "Cracked Vase", points: 1 },
+      { answer: "The Fool", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "The Wheel of Fortune", personalityType: "Bottomless Book", points: 1 },
+      { answer: "The Devil", personalityType: "Soul Muncher", points: 1 },
+      { answer: "The hanged Man", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "6",
-    question: "How do you spend your weekends?",
+    question: "Pick an Element:",
     choices: [
-      { answer: "Volunteering or helping others", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Competing in sports or games", personalityType: "Energetic Type", points: 1 },
-      { answer: "Relaxing at home", personalityType: "Dust Bunny", points: 1 },
-      { answer: "Pushing your limits, trying new things", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Water", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Earth", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Air", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Fire", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Fire", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Air", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Earth", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Water", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "7",
-    question: "Which of these describes your ideal vacation?",
+    question: "How would you most likely act at a party?",
     choices: [
-      { answer: "Relaxing in a quiet cabin", personalityType: "Dust Bunny", points: 1 },
-      { answer: "A thrilling adventure trip", personalityType: "Energetic Type", points: 1 },
-      { answer: "Volunteering in a community project", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "Competing in a challenging event", personalityType: "Soul Muncher", points: 1 }
+      { answer: "Chatting with anyone/everyone, meeting new people, flirting", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Observing the crowd from the snack bar, only speaking when spoken to", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Hiding in a secluded corner reading or playing on your phone", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Drinking and causing a scene with an old acquaintance/friend/ex", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Dancing, drinking, overall enjoying the environment and the people, having a great time", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "You were brought by a friend and have little interest in actually being there", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Rifling through the drawers/house/others belonging to get information on others", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Challenging other to  party/drinking games and making new friends", personalityType: "Crystal Bubble", points: 1 }
     ]
   },
   {
     _id: "8",
-    question: "How do you make important decisions?",
+    question: "What if your favorite type of music?",
     choices: [
-      { answer: "By considering how it affects others", personalityType: "Bleeding Heart", points: 1 },
-      { answer: "By following your instincts", personalityType: "Soul Muncher", points: 1 },
-      { answer: "By analyzing all the details", personalityType: "Dust Bunny", points: 1 },
-      { answer: "By taking quick, decisive action", personalityType: "Energetic Type", points: 1 }
+      { answer: "Heartbreak/love songs", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Punk", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Lofi", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Rap", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Pop", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Audiobooks", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Heavy metal", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Country", personalityType: "Crystal Bubble", points: 1 }
+    ]
+  },
+  {
+    _id: "9",
+    question: "What image speaks to you the most?",
+    choices: [
+      { answer: "A beautiful proposal", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Two kids fighting over a stuffed toy", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "A cat sleeping in a cozy corner", personalityType: "Dust Bunny", points: 1 },
+      { answer: "A shop filled with items (secondhand)", personalityType: "Cracked Vase", points: 1 },
+      { answer: "A fun birthday party/celebration", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "A vast, mystical library", personalityType: "Bottomless Book", points: 1 },
+      { answer: "A night with someone walking alone, a single streetlight above", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Pandoras box", personalityType: "Crystal Bubble", points: 1 }
+    ]
+  },
+  {
+    _id: "10",
+    question: "What environment speaks to you the most?",
+    choices: [
+      { answer: "The beach", personalityType: "Dust Bunny", points: 1 },
+      { answer: "The swamp", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "The forest", personalityType: "Dust Bunny", points: 1 },
+      { answer: "The tundra/mountains", personalityType: "Cracked Vase", points: 1 },
+      { answer: "The city", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "The desert", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Caverns/caves", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Grasslands", personalityType: "Crystal Bubble", points: 1 }
+    ]
+  },
+  {
+    _id: "11",
+    question: "What superpower do you wish you could have/find most useful to you?",
+    choices: [
+      { answer: "To charm anyone into doing what you want", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Mind reading", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Invisibility", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Top change the emotions of others at a whim", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Ability to fly/super speed", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "To have limitless knowledge – know the answer by thinking about it", personalityType: "Bottomless Book", points: 1 },
+      { answer: "To steal abilities from others", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Super strength", personalityType: "Crystal Bubble", points: 1 }
+    ]
+  },
+  {
+    _id: "12",
+    question: "What is your favorite time of day?",
+    choices: [
+      { answer: "Break of dawn", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Dusk", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Evening", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Night", personalityType: "Cracked Vase", points: 1 },
+      { answer: "All day", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Mid day", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Middle of the night", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Morning", personalityType: "Crystal Bubble", points: 1 }
+    ]
+  },
+  {
+    _id: "13",
+    question: "What would you do if you ruled the world?",
+    choices: [
+      { answer: "Spread love and kindness, removing war, ending hunger, world peace, and creating balance", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Build a strong empire with loyal subjects that do as you please", personalityType: "Prickly Pinecone", points: 1 },
+      { answer: "Run it all from the shadows – the people not knowing if you truly exist or not", personalityType: "Dust Bunny", points: 1 },
+      { answer: "Build statues and builds in your honor, making a few days a month to celebrate your greatness", personalityType: "Cracked Vase", points: 1 },
+      { answer: "Make more free time for family/friends/play and less work", personalityType: "Jumping jackalope", points: 1 },
+      { answer: "Create a highly advance and educated society", personalityType: "Bottomless Book", points: 1 },
+      { answer: "Burn everything to the ground", personalityType: "Soul Muncher", points: 1 },
+      { answer: "Build a strong foundation of equality and change to a prosperous future", personalityType: "Crystal Bubble", points: 1 }
     ]
   }
     // Add more questions as needed
